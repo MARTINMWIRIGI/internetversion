01183bfd378b6c26cd57de4c4fb6dd185ecc772b
MARTINMWIRIGI <imperialenterprisekenya@gmail.com>
2025-11-26 23:13:47 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",  // 🔥 allows API routes
   reactStrictMode: true,
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
 
   experimental: {
     serverActions: {
       allowedOrigins: ["*"],
     },
   },
-
-  // The important part for Cloudflare Pages
-  output: "export",
 };
 
 export default nextConfig;
