a9cdf3825e0b63f1248d715532f5fe93d4f695c2
MARTINMWIRIGI <imperialenterprisekenya@gmail.com>
2025-11-27 00:26:08 +0300

    Update mint.js

--- functions/api/mint.js
+++ functions/api/mint.js
@@ -1,10 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
 export async function onRequestPost({ request, env }) {
+  // Use env variables defined in Cloudflare Workers dashboard
   const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
 
   try {
     const body = await request.json();
+
+    // Insert submission into Supabase
     const { data, error } = await supabase.from("submissions").insert([body]);
     if (error) throw error;
 
