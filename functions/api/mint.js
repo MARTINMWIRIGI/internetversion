f63a7db94cd6cbcc97dc1dfcab3891c30a7505e5
MARTINMWIRIGI <imperialenterprisekenya@gmail.com>
2025-11-27 00:20:06 +0300

    Update mint.js

--- functions/api/mint.js
+++ functions/api/mint.js
@@ -1,11 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-);
+export async function onRequestPost({ request, env }) {
+  const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
 
-export async function onRequestPost({ request }) {
   try {
     const body = await request.json();
     const { data, error } = await supabase.from("submissions").insert([body]);
