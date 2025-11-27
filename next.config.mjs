d39f53f3e2cced6f97d914a2d2f1cb14a549921e
MARTINMWIRIGI <imperialenterprisekenya@gmail.com>
2025-11-27 17:00:35 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone', // required for server-side functions on Cloudflare
-  experimental: {
-    serverActions: true, // if using server actions
-  },
 };
 
 export default nextConfig;
