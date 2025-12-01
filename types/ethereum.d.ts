f9a30ca9fd9b044c745cc4a252143bcf9077f9a6
MARTINMWIRIGI <imperialenterprisekenya@gmail.com>
2025-12-01 22:36:47 +0300

    Create ethereum.d.ts

---
+++ types/ethereum.d.ts
@@ -0,0 +1,11 @@
+interface EthereumProvider {
+  isMetaMask?: boolean;
+  request: (args: { method: string; params?: any[] }) => Promise<any>;
+  on: (event: string, callback: (...args: any[]) => void) => void;
+  removeListener: (event: string, callback: (...args: any[]) => void) => void;
+  selectedAddress: string | null;
+}
+
+interface Window {
+  ethereum?: EthereumProvider;
+}
