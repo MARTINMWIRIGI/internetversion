18bc31a6de5c34194e46efacafb7dc957fec863f
MARTINMWIRIGI <imperialenterprisekenya@gmail.com>
2025-12-02 11:07:54 +0300

    Update BiometricFormSimple.tsx

--- components/BiometricFormSimple.tsx
+++ components/BiometricFormSimple.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { 
   Mic, Fingerprint, Brain, Shield, 
   MicOff, Lock, AlertCircle, CheckCircle,
