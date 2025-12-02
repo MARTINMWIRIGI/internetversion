2a273dc9b8de38413a375bd49c4f7f4493eee16d
MARTINMWIRIGI <imperialenterprisekenya@gmail.com>
2025-12-02 10:07:33 +0300

    Update CulturalFormReal.tsx

--- components/CulturalFormReal.tsx
+++ components/CulturalFormReal.tsx
@@ -98,7 +98,7 @@ export default function CulturalFormReal() {
               Saving to Database...
             </span>
           ) : (
-            '✨ Save & Mint to Blockchain'
+            '✨ Save to Mint'
           )}
         </button>
         
