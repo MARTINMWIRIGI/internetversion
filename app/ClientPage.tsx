2b06185e0454db5fbb3ec88ad1990700ad4d51d5
MARTINMWIRIGI <imperialenterprisekenya@gmail.com>
2025-12-06 08:08:55 +0300

    Update ClientPage.tsx

--- app/ClientPage.tsx
+++ app/ClientPage.tsx
@@ -101,6 +101,7 @@ export default function SoulInternetHome() {
         </div>
       </motion.div>
     );
+}
   if (!mounted) return (
     <div className="min-h-screen bg-black flex items-center justify-center">
       <div className="relative">
